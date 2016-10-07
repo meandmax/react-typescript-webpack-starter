fdd3883e543445bb4112e76c2da015f35a810884
Maximilian Heinz <max.heinz@jimdo.com>
2016-10-07 12:46:37 +0200

    remove requirejs because we must use the webpack types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,5 @@
 /// <reference path="globals/classnames/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
-/// <reference path="globals/require/index.d.ts" />
 /// <reference path="globals/webpack-env/index.d.ts" />
 /// <reference path="globals/webpack/index.d.ts" />
